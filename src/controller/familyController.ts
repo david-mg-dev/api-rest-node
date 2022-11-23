a71b4da5dc22810729b35c91754cf611f5256369
David Morales <david.dmg89@gmail.com>
2022-11-23 12:52:00 +0100

    feat: add error.handle

--- src/controller/familyController.ts
+++ src/controller/familyController.ts
@@ -6,6 +6,7 @@ const getRedirect = (_req: Request, res: Response) => {
   const url = 'http://localhost:5003/api/v1/family'
   res.send(`<a href="${url}">Pulsa aqui</a> para url correcta`)
 }
+
 const getIndex = (_req: Request, res: Response) => {
   try {
     res.send(familyService.getFamilia())
